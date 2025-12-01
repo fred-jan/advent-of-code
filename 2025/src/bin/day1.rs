5e22d70953046865766b06888c0f5789087fde1a
Fred-Jan van der Eijken <2146611+fred-jan@users.noreply.github.com>
2025-12-01 14:35:13 +0100

    Added 2025 day 1 part 2

--- 2025/src/bin/day1.rs
+++ 2025/src/bin/day1.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fmt::Display, fs};
 
 struct Problem {
     safe: Safe,
@@ -12,7 +12,11 @@ impl Problem {
     }
 
     pub fn part_1(&self) -> u32 {
-        self.safe.password()
+        self.safe.do_rotations().zero_ends
+    }
+
+    pub fn part_2(&self) -> u32 {
+        self.safe.do_rotations().zero_clicks
     }
 }
 
@@ -29,21 +33,10 @@ impl Safe {
         }
     }
 
-    pub fn password(&self) -> u32 {
-        let (_dial, times_zero) = self.rotations.iter().fold(
-            (self.dial.clone(), 0),
-            |(dial, mut times_zero), rotation| {
-                let dial = dial.interact(rotation);
-
-                if dial.position == 0 {
-                    times_zero += 1;
-                }
-
-                (dial, times_zero)
-            },
-        );
-
-        times_zero
+    pub fn do_rotations(&self) -> Dial {
+        self.rotations
+            .iter()
+            .fold(self.dial.clone(), |dial, rotation| dial.interact(rotation))
     }
 }
 
@@ -51,18 +44,50 @@ impl Safe {
 struct Dial {
     size: u32,
     position: u32,
+    zero_clicks: u32,
+    zero_ends: u32,
 }
 
 impl Dial {
     pub fn interact(&self, rotation: &Rotation) -> Self {
+        // Full revolutions
+        let revolutions = rotation.distance / self.size;
+
+        // Remainder distance
+        let rem_distance = rotation.distance % self.size;
+
+        // Net distance to be added (normalizes direction)
+        let add_distance = match rotation.direction {
+            // Subtracting D mod X is the same as adding X-D mod X
+            Direction::Left => self.size - rem_distance,
+            Direction::Right => rem_distance,
+        };
+
+        let new_position = (self.position + add_distance) % self.size;
+
+        let extra_rev = self.position != 0
+            && match rotation.direction {
+                Direction::Left => new_position > self.position,
+                Direction::Right => new_position < self.position,
+            };
+
+        let mut zero_clicks = self.zero_clicks + revolutions;
+
+        // Add zero click ending at zero, or when starting from non-zero and making a revolution
+        if new_position == 0 || extra_rev {
+            zero_clicks += 1;
+        }
+
+        // println!(
+        //     "Old pos: {}, Rotation: {}, New pos: {}, Revolutions: {}, Zero clicks: {}",
+        //     self.position, rotation, new_position, revolutions, zero_clicks
+        // );
+
         Self {
             size: self.size,
-            position: match rotation {
-                Rotation::Left(distance) => {
-                    (self.position + self.size - (distance % self.size)) % self.size
-                }
-                Rotation::Right(distance) => (self.position + distance) % self.size,
-            },
+            position: new_position,
+            zero_clicks,
+            zero_ends: self.zero_ends + if new_position == 0 { 1 } else { 0 },
         }
     }
 }
@@ -72,30 +97,62 @@ impl Default for Dial {
         Self {
             size: 100,
             position: 50,
+            zero_clicks: 0,
+            zero_ends: 0,
         }
     }
 }
 
-enum Rotation {
-    Left(u32),
-    Right(u32),
+enum Direction {
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub fn from_char(chr: &char) -> Self {
+        match chr {
+            'L' => Self::Left,
+            'R' => Self::Right,
+            _ => panic!("Unsupported rotation direction"),
+        }
+    }
+}
+
+impl Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Left => 'L',
+                Self::Right => 'R',
+            }
+        )
+    }
+}
+
+struct Rotation {
+    direction: Direction,
+    distance: u32,
 }
 
 impl Rotation {
     pub fn from_string(string: &str) -> Self {
         let mut chars = string.chars();
 
-        let direction = chars.next().expect("Missing direction letter");
-        let distance = chars
-            .as_str()
-            .parse::<u32>()
-            .expect("Unable to parse rotation distance as unsigned int");
-
-        match direction {
-            'L' => Self::Left(distance),
-            'R' => Self::Right(distance),
-            _ => panic!("Unsupported rotation direction"),
+        Self {
+            direction: Direction::from_char(&chars.next().expect("Missing direction letter")),
+            distance: chars
+                .as_str()
+                .parse::<u32>()
+                .expect("Unable to parse rotation distance as unsigned int"),
         }
+    }
+}
+
+impl Display for Rotation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}", self.direction, self.distance)
     }
 }
 
@@ -106,6 +163,7 @@ fn main() {
             .as_str(),
     );
     println!("Part 1: {}", problem.part_1()); // Attempts: 33, 989
+    println!("Part 2: {}", problem.part_2()); // Attempts: 5949, 5941
 }
 
 #[cfg(test)]
@@ -126,5 +184,10 @@ L82"#;
     #[test]
     fn test_sample_part_1() {
         assert_eq!(3, Problem::from_string(SAMPLE).part_1());
+    }
+
+    #[test]
+    fn test_sample_part_2() {
+        assert_eq!(6, Problem::from_string(SAMPLE).part_2());
     }
 }
