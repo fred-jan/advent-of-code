b1ac7ed077699a3ee1fd9154fea4aa0007760b1a
Fred-Jan van der Eijken <2146611+fred-jan@users.noreply.github.com>
2025-12-07 10:06:19 +0100

    Added 2025 day 7 part 2

--- 2025/src/bin/day7.rs
+++ 2025/src/bin/day7.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs, str::FromStr, vec};
+use std::{collections::HashMap, error::Error, fs, str::FromStr, vec};
 
 struct Problem {
     diagram: Diagram,
@@ -16,7 +16,12 @@ impl FromStr for Problem {
 
 impl Problem {
     pub fn part_1(&self) -> usize {
-        self.diagram.split_count()
+        self.diagram.count_splits()
+    }
+
+    pub fn part_2(&self) -> usize {
+        self.diagram
+            .count_timelines(self.diagram.start, &mut HashMap::new())
     }
 }
 
@@ -64,18 +69,56 @@ impl FromStr for Diagram {
 }
 
 impl Diagram {
-    fn split_count(&self) -> usize {
+    fn count_timelines(
+        &self,
+        loc_particle: Location,
+        loc_counts: &mut HashMap<Location, usize>,
+    ) -> usize {
+        // Check the cache to prevent computing the timelines for this particle position again
+        if let Some(timeline_count) = loc_counts.get(&loc_particle) {
+            return *timeline_count;
+        }
+
+        for j in loc_particle.1..self.height {
+            // Find splitter hit (if any) on current row, otherwise go to next row
+            match self
+                .row_splitter_xs(j)
+                .into_iter()
+                .find(|splitter_x| *splitter_x == loc_particle.0)
+            {
+                // Splitter hit by the particle, count timelines on traversable branches
+                Some(_) => {
+                    let mut timeline_count = 0;
+
+                    if loc_particle.0.checked_sub(1).is_some() {
+                        timeline_count +=
+                            self.count_timelines((loc_particle.0 - 1, j + 1), loc_counts)
+                    }
+                    if loc_particle.0 + 1 < self.width {
+                        timeline_count +=
+                            self.count_timelines((loc_particle.0 + 1, j + 1), loc_counts)
+                    }
+
+                    // Cache the number of timelines for this particle position
+                    loc_counts.insert(loc_particle, timeline_count);
+
+                    return timeline_count;
+                }
+                // If no splitter was hit by the particle, traverse to next row
+                None => continue,
+            };
+        }
+
+        // End reached, count as one timeline
+        1
+    }
+
+    fn count_splits(&self) -> usize {
         let mut beam_xs = vec![self.start.0];
         let mut split_count = 0;
 
         for j in 1..self.height {
-            let splitter_xs = self
-                .splitters
-                .iter()
-                .copied()
-                .filter(|loc| loc.1 == j)
-                .map(|loc| loc.0)
-                .collect::<Vec<usize>>();
+            let splitter_xs = self.row_splitter_xs(j);
 
             if splitter_xs.is_empty() {
                 continue;
@@ -110,12 +153,22 @@ impl Diagram {
 
         split_count
     }
+
+    fn row_splitter_xs(&self, j: usize) -> Vec<usize> {
+        self.splitters
+            .iter()
+            .copied()
+            .filter(|loc| loc.1 == j)
+            .map(|loc| loc.0)
+            .collect::<Vec<usize>>()
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let problem = fs::read_to_string("input/day7.txt")?.parse::<Problem>()?;
 
     println!("Part 1: {}", problem.part_1()); // Attempts: 1622
+    println!("Part 2: {}", problem.part_2()); // Attempts: 10357305916520
 
     Ok(())
 }
@@ -146,5 +199,10 @@ mod tests {
     #[test]
     fn test_sample_part_1() {
         assert_eq!(21, SAMPLE.parse::<Problem>().unwrap().part_1());
+    }
+
+    #[test]
+    fn test_sample_part_2() {
+        assert_eq!(40, SAMPLE.parse::<Problem>().unwrap().part_2());
     }
 }
